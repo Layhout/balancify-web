5c71b21286e01883bc221cba74867de15ffa7cd4
layhout.chea <layhout.chea@wingbank.com.kh>
2025-05-09 13:58:39 +0700

    chore: wip

--- app/app/expenses/create/page.tsx
+++ app/app/expenses/create/page.tsx
@@ -1,40 +1,16 @@
 'use client'
 
-import { ExpanseAvatar } from '@/components/ExpanseAvatar'
 import { PageHeader } from '@/components/PageHeader'
-import { Label } from '@/components/ui/label'
-import { BG_COLORS, EXPENSE_ICONS } from '@/lib/constants'
+import AvatarForm from './_components/AvatarForm'
+import InfoForm from './_components/InfoForm'
 
 export default function CreateExpenses() {
   return (
     <div className="container pb-4">
       <PageHeader title="Create Expense" hasBackBtn />
       <div className="mt-6 flex gap-6">
-        <div className="flex flex-[0.3] flex-col gap-6">
-          <ExpanseAvatar
-            icon={Object.values(EXPENSE_ICONS)[Math.floor(Math.random() * (Object.values(EXPENSE_ICONS).length - 1))]}
-            iconBgColor={BG_COLORS[Math.floor(Math.random() * (BG_COLORS.length - 1))]}
-            className="h-32 w-32"
-            iconClassName="h-20 w-20"
-          />
-          <div className="flex-1">
-            <Label>Icons</Label>
-            <div className="mt-4 flex flex-wrap gap-4">
-              {Object.values(EXPENSE_ICONS).map((ei, i) => (
-                <img key={i} src={`/assets/svgs/icon-${ei}.svg`} className={'h-8 w-8'} alt="icon" />
-              ))}
-            </div>
-          </div>
-          <div className="flex-1">
-            <Label>Colors</Label>
-            <div className="mt-4 flex flex-wrap gap-4">
-              {BG_COLORS.map((c, i) => (
-                <div key={i} className="h-8 w-8 rounded-lg" style={{ backgroundColor: c }} />
-              ))}
-            </div>
-          </div>
-        </div>
-        <div className="flex-1"></div>
+        <AvatarForm />
+        <InfoForm />
       </div>
     </div>
   )
--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,25 +1,44 @@
-import * as React from "react"
+import * as React from 'react'
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
+import { IconType } from 'react-icons/lib'
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  startIcon?: IconType
+  endIcon?: IconType
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, startIcon, endIcon, ...props }, ref) => {
+    const StartIcon = startIcon
+    const EndIcon = endIcon
+
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-          className
+      <div className="relative w-full">
+        {StartIcon && (
+          <div className="absolute left-1.5 top-1/2 -translate-y-1/2 transform">
+            <StartIcon size={18} className="text-muted-foreground" />
+          </div>
         )}
-        ref={ref}
-        {...props}
-      />
+        <input
+          type={type}
+          className={cn(
+            'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+        {EndIcon && (
+          <div className="absolute right-3 top-1/2 -translate-y-1/2 transform">
+            <EndIcon className="text-muted-foreground" size={18} />
+          </div>
+        )}
+      </div>
     )
-  }
+  },
 )
-Input.displayName = "Input"
+
+Input.displayName = 'Input'
 
 export { Input }
