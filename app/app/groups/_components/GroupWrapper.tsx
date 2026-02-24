64f4a8d7d3cd86b12056ce69c3ba8ef12efcf185
Layhout <layhoutworkmail@gmail.com>
2026-02-24 10:19:56 +0700

    chore: imp

--- app/app/groups/_components/GroupWrapper.tsx
+++ app/app/groups/_components/GroupWrapper.tsx
@@ -10,7 +10,7 @@ export function GroupWrapper({
   return (
     <ul className="flex w-full flex-col gap-4">
       <li className="flex items-center gap-4 px-4 py-0">
-        <h1 className="flex-[0.6] text-sm lg:flex-[0.4]">Name</h1>
+        <h1 className="hidden flex-[0.4] text-sm lg:block">Name</h1>
         <h1 className="hidden flex-[0.4] text-sm lg:block">Members</h1>
         <h1 className="hidden flex-[0.2] text-sm lg:block">Created</h1>
       </li>
